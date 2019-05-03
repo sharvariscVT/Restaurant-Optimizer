7b684f1a60416e5ec6230aa5eb79e9605b02726d
Ashish Kotian <45375711+ashishshk@users.noreply.github.com>
2019-05-02 22:13:02 -0400

    Create SuggestionsController.java

---
+++ src/java/edu/vt/controllers/SuggestionsController.java
@@ -0,0 +1,171 @@
+/*
+ * Created by Ashish Kotian on 2019.05.03
+ * Copyright © 2019 Ashish Kotian. All rights reserved.
+ */
+package edu.vt.controllers;
+
+import edu.vt.EntityBeans.Suggestions;
+import edu.vt.controllers.util.JsfUtil;
+import edu.vt.controllers.util.JsfUtil.PersistAction;
+import edu.vt.FacadeBeans.SuggestionsFacade;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@Named("suggestionsController")
+@SessionScoped
+public class SuggestionsController implements Serializable {
+
+    @EJB
+    private edu.vt.FacadeBeans.SuggestionsFacade ejbFacade;
+    private List<Suggestions> items = null;
+    private Suggestions selected;
+
+    public SuggestionsController() {
+    }
+
+    public Suggestions getSelected() {
+        System.out.println("Geeeeetttttwhateva");
+        System.out.println(selected);
+        return selected;
+    }
+
+    public void setSelected(Suggestions selected) {
+        this.selected = selected;
+    }
+
+    protected void setEmbeddableKeys() {
+    }
+
+    protected void initializeEmbeddableKey() {
+    }
+
+    private SuggestionsFacade getFacade() {
+        return ejbFacade;
+    }
+
+    public Suggestions prepareCreate() {
+        selected = new Suggestions();
+        initializeEmbeddableKey();
+        return selected;
+    }
+
+    public void create() {
+        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("SuggestionsCreated"));
+        if (!JsfUtil.isValidationFailed()) {
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public void update() {
+        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("SuggestionsUpdated"));
+    }
+
+    public void destroy() {
+        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("SuggestionsDeleted"));
+        if (!JsfUtil.isValidationFailed()) {
+            selected = null; // Remove selection
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public List<Suggestions> getItems() {
+        if (items == null) {
+            items = getFacade().findAll();
+        }
+        return items;
+    }
+
+    private void persist(PersistAction persistAction, String successMessage) {
+        if (selected != null) {
+            setEmbeddableKeys();
+            try {
+                if (persistAction != PersistAction.DELETE) {
+                    getFacade().edit(selected);
+                } else {
+                    getFacade().remove(selected);
+                }
+                JsfUtil.addSuccessMessage(successMessage);
+            } catch (EJBException ex) {
+                String msg = "";
+                Throwable cause = ex.getCause();
+                if (cause != null) {
+                    msg = cause.getLocalizedMessage();
+                }
+                if (msg.length() > 0) {
+                    JsfUtil.addErrorMessage(msg);
+                } else {
+                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            }
+        }
+    }
+
+    public Suggestions getSuggestions(java.lang.String id) {
+        return getFacade().find(id);
+    }
+
+    public List<Suggestions> getItemsAvailableSelectMany() {
+        return getFacade().findAll();
+    }
+
+    public List<Suggestions> getItemsAvailableSelectOne() {
+        return getFacade().findAll();
+    }
+
+    @FacesConverter(forClass = Suggestions.class)
+    public static class SuggestionsControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            SuggestionsController controller = (SuggestionsController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "suggestionsController");
+            return controller.getSuggestions(getKey(value));
+        }
+
+        java.lang.String getKey(String value) {
+            java.lang.String key;
+            key = value;
+            return key;
+        }
+
+        String getStringKey(java.lang.String value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Suggestions) {
+                Suggestions o = (Suggestions) object;
+                return getStringKey(o.getSuggestion());
+            } else {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Suggestions.class.getName()});
+                return null;
+            }
+        }
+
+    }
+
+}
