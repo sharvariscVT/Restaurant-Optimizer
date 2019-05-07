d4a50b2535cf3ed6729e1ae7b95faa5066656f42
sharvariscVT <47427623+sharvariscVT@users.noreply.github.com>
2019-05-06 23:20:17 -0400

    Update Constants.java

--- src/java/edu/vt/globals/Constants.java
+++ src/java/edu/vt/globals/Constants.java
@@ -39,7 +39,7 @@ public final class Constants {
     macOS (Unix or Linux): Specify the absolute directory path as shown below.
      */
     public static final String FILES_ABSOLUTE_PATH  = "C:/Users/Chougule/DocRoot/CloudStorage/";
-    public static final String PHOTOS_ABSOLUTE_PATH = "C:/Users/Chougule/DocRoot/CloudStorage/";
+    public static final String PHOTOS_ABSOLUTE_PATH = "C:/Users/Chougule/DocRoot/CloudStorage/PhotoStorage/";
 
     
     
